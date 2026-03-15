84c2e62a8f16f4cddc1273b30afa26ab63820ca4
AungHtweMaung <aunghtwemaung121@gmail.com>
2026-03-15 10:16:55 +0630

    change title text in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Restaurant Order Management</title>
+    <title>Rangoon Food House Admin</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset('src/assets/vendors/feather/feather.css') }}">
     {{-- <link rel="stylesheet" href="{{ asset('src/assets/vendors/ti-icons/css/themify-icons.css') }}"> --}}
@@ -63,18 +63,6 @@
         </div>
 
         @include('layouts.bs-toast-message')
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
-        {{-- <footer class="footer">
-                <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2025.
-                        <a href="#" target="_blank">AUSphere</a>. All rights reserved.</span>
-                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
-                        <i class="ti-heart text-danger ms-1"></i></span>
-                </div>
-            </footer> --}}
-        <!-- partial -->
-        {{-- </div> --}}
         <!-- main-panel ends -->
     </div>
     <!-- page-body-wrapper ends -->
